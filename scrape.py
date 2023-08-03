33680dd749b8359f2d701d4311ab89df575ea35f
thealphacubicle <srihariraman9@gmail.com>
2023-08-03 16:14:54 -0400

    Created CSV data file and analysis IPYNB

--- scrape.py
+++ scrape.py
@@ -54,36 +54,7 @@ def add_reviews_to_dict(start, end, base_url, tag_type, review_dct, class_names=
                               class_names=class_names, attrs_=attrs)
         if result:
             for span in result:
-                if span.text not in stop_list and len(span.text) > 3:
+                if span.text not in stop_list and len(span.text.split()) > 3:
                     r.append(span.text)
         review_dct[website] = r
 
-###____________________________________________________________________TO PUT IN ANALYSIS FILE________________________________________________________________###
-stops = ['2 S Station', 'Boston, MA 02111', 'Yelp users haven’t asked any questions yet about Amtrak.', 'Start your review of Amtrak.']
-reviews = {}
-
-# # Scrape reviews off Yelp and add valid reviews to list
-add_reviews_to_dict(0, 90, step=10, base_url='https://www.yelp.com/biz/amtrak-boston-3?start={}',
-                    tag_type='span', class_names='raw__09f24__T4Ezm', stop_list=stops, review_dct=reviews)
-
-# # Loop through each page fo Trustpilot and add valid reviews to the list
-add_reviews_to_dict(1, 30, base_url="https://www.trustpilot.com/review/www.amtrak.com?page={}",
-                              tag_type='p', review_dct=reviews,
-                              class_names='typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn',
-                              stop_list=stops,
-                              attrs={'data-service-review-text-typography': 'true'})
-
-# Loop through each page fo Viewpoints.com and add valid reviews to the list
-add_reviews_to_dict(0, 1, base_url="https://www.viewpoints.com/Amtrak-Train-TRavel-reviews",
-                              tag_type='p', review_dct=reviews,
-                              class_names=['pr-review-faceoff-review', 'pr-comments', 'pr-review-faceoff-review-full'],
-                              stop_list=stops)
-
-# Loop through each page fo Reddit.com and add valid reviews to the list
-add_reviews_to_dict(0, 1, base_url="https://www.sitejabber.com/reviews/amtrak.com",
-                              tag_type='p', review_dct=reviews,
-                              stop_list=stops)
-
-
-# Get review data statistics
-print_review_count(review_dict=reviews)
